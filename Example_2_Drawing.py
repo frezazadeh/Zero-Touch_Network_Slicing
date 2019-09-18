89ffbe1ee28565f09b5136bfc806914229b87cad
Farhad Rezazadeh <47423984+Farhad-UPC@users.noreply.github.com>
2019-09-18 09:49:41 +0200

    Delete Example_2_Drawing.py

--- Example_2_Drawing.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-
-G=nx.cubical_graph() #Return the 3-regular Platonic Cubical graph.
-plt.subplot(121) #subplot(nrows, ncols, index)
-nx.draw(G) # default spring_layout
-
-plt.subplot(122)
-nx.draw(G, pos=nx.circular_layout(G), nodecolor='r', edge_color='b')
